7b375addf17f422e80653cc345b706b104d998da
Eggert Jóhannesson <eggert@hugsaser.is>
2014-02-25 16:00:44 +0000

    Added chatserver.js to repo. Some cleaning up also

--- dev/js/filters/Count.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('ColossalChat')
-.filter('count', [function(){
-    return function(obj) {
-        return _.size(angular.copy(obj));
-    };
-}]);
--- dev/js/services/Settings.js
+++
@@ -1,7 +0,0 @@
-/*globals angular */
-'use strict';
-
-angular.module('ColossalChat')
-.constant('Settings', {
-    BACKEND_URL: 'http://localhost:8080'
-});
